6ec9c5596fbf870aed99d5ec3d2e02fa55fd48fa
dimaliwatkent <dimaliwatkent@gmail.com>
2023-12-17 09:37:33 +0800

    .

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# book-archive
+CREATE TABLE books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    category VARCHAR(255),
+    publication_date DATE,
+    publisher VARCHAR(255),
+    description TEXT
+);
--- index.php
+++ index.php
@@ -7,7 +7,16 @@
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
+<form action="php/insert_book.php" method="post">
+      <input type="text" name="title" placeholder="Title" required>
+      <input type="text" name="author" placeholder="Author" required>
+      <input type="text" name="category" placeholder="Category">
+      <input type="date" name="publication_date" placeholder="Publication Date">
+      <input type="text" name="publisher" placeholder="Publisher">
+      <textarea name="description" placeholder="Description"></textarea>
+      <input type="submit" value="Add Book">
+  </form>
    
     <script src="js/index.js"></script>
-</body>
+</body> 
 </html>
